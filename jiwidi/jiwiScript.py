b8a733955abe59d128a21940e799807b72337c53
Jaime Ferrando Huertas <fhjaime96@gmail.com>
2018-02-01 13:03:48 +0100

    Add files via upload

--- jiwidi/jiwiScript.py
+++ jiwidi/jiwiScript.py
@@ -1,11 +1,16 @@
 import pandas as pd
+import numpy as np
 import tensorflow as tf
 import csv
 
+
 LEARNING_RATE = 0.001
 DROPOUT = 0.1
+EPOCH = 5
+
 TRAIN = pd.read_csv('../data/train.txt')
 TEST = pd.read_csv('../data/test.txt')
+
 TARGET = TRAIN["Poder_Adquisitivo"].as_matrix()
 TRAIN = TRAIN.drop("ID_Customer", axis=1).drop("Poder_Adquisitivo", axis=1)
 TEST = TEST.drop("ID_Customer", axis=1)
@@ -15,49 +20,38 @@ for l in categorical:
     TRAIN = TRAIN.drop(l,axis=1)
     TEST = TEST.drop(l, axis=1)
 
-def main(unsued_argv):
-    # Set model params
-    model_params = {"learning_rate": LEARNING_RATE,
-                    "dropout": DROPOUT}
 
-    #Fit the model
-    nn = tf.contrib.learn.Estimator(
-        model_fn=model_fn, params=model_params)
-    nn.fit(x=TRAIN.as_matrix(), y=TEST.as_matrix(), steps=200000)
+# Input builders
+def input_fn_train(): # returns x, y
+    return tf.convert_to_tensor(np.array(TRAIN)),tf.convert_to_tensor(np.array(TARGET))
 
-def model_fn(features, targets, mode, params):
-    """Model function for Estimator."""
 
-    # Connect the first hidden layer to input layer
-    # (features) with relu activation
-    first_hidden_layer = tf.contrib.layers.relu(features, 82)
+def input_fn_eval(): # returns x, y
+  pass
+#metrics = estimator.evaluate(input_fn=input_fn_eval, steps=10)
+def input_fn_predict(): # returns x, None
+  pass
 
-    drop_out1 = tf.nn.dropout(first_hidden_layer, params['dropout'])  # DROP-OUT here
 
-    # Connect the second hidden layer to first hidden layer with relu
-    second_hidden_layer = tf.contrib.layers.relu(drop_out1, 50)
+feature_cols = [tf.contrib.layers.real_valued_column("", dimension=2)]
 
-    drop_out2 = tf.nn.dropout(second_hidden_layer, params['dropout'])
 
-    # Connect the output layer to second hidden layer (no activation fn)
-    output_layer = tf.contrib.layers.linear(drop_out2, 1)
+# Model
+tf.logging.set_verbosity(tf.logging.ERROR)
+regressor = tf.contrib.learn.DNNRegressor(feature_columns=feature_cols,
+                                          activation_fn = tf.nn.relu, hidden_units=[200, 100, 50, 25, 12])#,
+                                         #optimizer = tf.train.GradientDescentOptimizer( learning_rate= 0.1 ))
 
-    # Reshape output layer to 1-dim Tensor to return predictions
-    predictions = tf.reshape(output_layer, [-1])
-    predictions_dict = {"y": predictions}
+# Deep Neural Network Regressor with the training set which contain the data split by train test split
+regressor.fit(input_fn=lambda: input_fn_train(), steps=10)
 
-    # Calculate loss using mean squared error
-    loss = tf.contrib.losses.mean_squared_error(predictions, targets)
-    train_op = tf.contrib.layers.optimize_loss(
-      loss=loss,
-      global_step=tf.contrib.framework.get_global_step(),
-      learning_rate=params["learning_rate"],
-      optimizer="SGD")
+# Evaluation on the test set created by train_test_split
+ev = regressor.evaluate(input_fn=lambda: input_fn_train(), steps=1)
+loss_score1 = ev["loss"]
+print("Final Loss on the testing set: {0:f}".format(loss_score1))
 
-    return predictions_dict, loss, train_op
 
-if __name__ == "__main__":
-  tf.app.run()
+
 
 
 
